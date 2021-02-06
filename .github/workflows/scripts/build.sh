84d8a22ea87467d9c16edf5f27a89c3d103075f3
David Guida <david.guida.03@gmail.com>
2021-02-06 13:58:39 -0500

    updated build script

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -7,9 +7,10 @@ rm -rf "build"
 echo "building $projectName ..."
 buildPath="build/$projectName-tmp"    
 dotnet publish $projectFile --output $buildPath --configuration Debug
-mv $buildPath/wwwroot/ "build/"        
-rm -rf $buildPath
 
-sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/$solutionName\/\" \/>/g" build/wwwroot/index.html
+cp -Rp $buildPath/wwwroot/ build      
+ rm -rf $buildPath
+
+sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/$solutionName\/\" \/>/g" build/index.html
 
 cp readme.md build/readme.md

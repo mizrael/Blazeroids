105920bb4318528147aad4e734812e160bd167c1
David Guida <david.guida.03@gmail.com>
2021-02-06 13:14:01 -0500

    added github pages build script

---
+++ .github/workflows/scripts/build.sh
@@ -0,0 +1,13 @@
+projects=($(ls -1 -- **/*.csproj))
+for i in "${projects[@]}"
+do
+    filename=$(basename -- "$i")    
+    filename="${filename%.*}"
+    buildPath="build/$filename-t"    
+	dotnet publish --configuration Release $i --output $buildPath
+    mv $buildPath/wwwroot/ "build/$filename"        
+    rm -rf $buildPath
+
+    sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/Blazeroids\/$filename\/\" \/>/g" build/$filename/index.html
+done
+cp readme.md build/readme.md

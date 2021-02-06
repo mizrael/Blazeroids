ef7886d91eb10b0f0daafaf1363edefea0a024af
David Guida <david.guida.03@gmail.com>
2021-02-06 14:08:45 -0500

    updated paths

--- .github/workflows/scripts/build.sh
+++ .github/workflows/scripts/build.sh
@@ -2,15 +2,15 @@ solutionName="Blazeroids"
 projectName="Blazeroids.Web"
 projectFile="$projectName/$projectName.csproj"
 
-rm -rf "build"
+rm -rf ./build
 
 echo "building $projectName ..."
-buildPath="build/$projectName-tmp"    
+buildPath="./build/$projectName-tmp"    
 dotnet publish $projectFile --output $buildPath --configuration Debug
 
-cp -Rp $buildPath/wwwroot/ build      
- rm -rf $buildPath
+cp -Rp $buildPath/wwwroot/ ./build      
+rm -rf $buildPath
 
-sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/$solutionName\/\" \/>/g" build/index.html
+sed -i -e "s/<base href=\"\/\" \/>/<base href=\"\/$solutionName\/\" \/>/g" ./build/index.html
 
 cp readme.md build/readme.md
